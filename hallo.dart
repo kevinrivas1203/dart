a061c201870a951b81b8520b8761fb2e617f56d0
kevinrivas1203 <kevin_rivas@teilnehmer.cdemy.de>
2026-03-27 09:12:33 +0100

    Hola Fertig Co-authored-by: cdemy Leitung <cdemyLeitung@users.noreply.github.com>

--- hallo.dart
+++ hallo.dart
@@ -1,3 +1,50 @@
-void main(List<String> arguments) {
-  print('Hallo '+ + arguments.toString()); 
+void main3(List<String> arguments){
+  if (arguments.isEmpty) {
+    print ('Hallo!');
+  } else  {
+    print('Hallo ${arguments[0]}!');
+    }
 }
+
+void main2(List<String> arguments) {
+  if (arguments.isNotEmpty) {
+    print('Hallo ${arguments[0]}!');
+  } else {
+    print ('Hallo!');
+  }
+}
+
+void main4(List<String> arguments) {
+  if (arguments.isNotEmpty) {
+    final joined = arguments.join(' ');
+    print('Hallo $joined!');
+  } else {
+    print ('Hallo!');
+  }
+}
+
+void main(List<String> arguments) {
+  if (arguments.isNotEmpty) {
+    String joined = arguments.first;
+    for (var i = 1; i < arguments.length; i++) {
+      joined += ' ';
+      joined += arguments[i];
+    }
+    print('Hallo $joined!');
+  } else {
+    print ('Hallo!');
+  }
+}
+
+
+// dart run hello.dart Welt
+// ['Welt']
+
+// dart run hello.dart "Kevin Rivas"
+// ['Kevin Rivas']
+
+// dart run hello.dart Kevin Rivas
+// ['Kevin', 'Rivas']
+
+// dart run hello.dart Kevin Rivas Vega
+// ['Kevin', 'Rivas', 'Vega']
