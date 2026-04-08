8fff275385f1e2ed586ceeb6cc7857a7afa2efde
kevinrivas1203 <kevin_rivas@teilnehmer.cdemy.de>
2026-04-08 10:28:44 +0200

    nada

--- wurfel2.dart
+++ wurfel2.dart
@@ -5,18 +5,21 @@ void main() {
   int? zahl;
   while (zahl == null) {
     print('anzahl Wurfel');
-    final wert = stdin.readLineSync();
-    zahl = int.tryParse(wert ?? '');
-    if (zahl == null) {
+    final wert = stdin.readLineSync(); // lee lo que el usuario escribe y lo guarda en una variable
+    zahl = int.tryParse(wert ?? '');    /*  zalh tiene un valor y es la transformacion de de lo
+                                         que escribe el usuario en String y lo convierte en numero.  
+                                        */
+
+      if (zahl == null) {
       print('Eingabe ungültig!');
     }
   }
-  final rnd = Random();
+  final rnd = Random(); //creo una variable que son numeros aleatorios
   print('Ziel erreicht 2 mal ergiebness 6: $zahl');
-  final alleWurfel = <int>[];
+  final alleWurfel = <int>[]; // crea una lista [] vacia
   for (var i = 0; i < zahl; i++) {
-    final wurf = rnd.nextInt(6) + 1;
-    alleWurfel.add(wurf);
+    final wurf = rnd.nextInt(6) + 1; //genera un numero aleatorio de 0 hasta 6 "dado de 6 caras" [0 1 2 3..] con el +1 le de digo que comienze a partir de 1 :1 2 3 ..
+    alleWurfel.add(wurf); 
     if (wurf == 6 && i > 0) {
       final vorherigerWert = alleWurfel[i-1];
       if (vorherigerWert == 6) {
